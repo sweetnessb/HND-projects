2e12492e4af6b5ce2fb2c5f41aa8f99141e88a1d
sweetnessb <145787164+sweetnessb@users.noreply.github.com>
2024-07-04 14:24:08 +0100

    Add files via upload
    stage one

--- hnd11 1/main.js
+++ hnd11 1/main.js
@@ -11,3 +11,29 @@ const emailElement = document.querySelector('[data-testid="slackEmail"]');
 const email = emailElement.getAttribute("data-email");
 
 emailElement.textContent = email;
+
+function getCurrentDateTime() {
+  const now = new Date();
+
+  // Formatting the date and time
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "short",
+  };
+
+  const dateTimeString = now.toLocaleDateString("en-US", options);
+
+  const dateTimeElement = document.querySelector('[data-testid="currentDay"]');
+
+  dateTimeElement.innerHTML = dateTimeString;
+}
+
+getCurrentDateTime();
+
+setInterval(getCurrentDateTime, 1000);
